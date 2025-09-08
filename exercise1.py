3cc099af2027a5f303da90886607e7975eee9001
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-08 15:28:18 -0300

    Input a new condicional for childrens

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+# Create a code that receives a person's age and indicates the movie they can watch according to their age
+
+age = int(input(" Digite a sua idade:"))
+
+if age < 12:
+    print("Você pode assistir ao filme Lilo & Stich")
