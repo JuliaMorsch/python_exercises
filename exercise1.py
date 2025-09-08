5d8fb50d411a53323d02553c56df93c0e87095d6
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-08 15:44:42 -0300

    Add a quantities of tickts

--- exercise1.py
+++ exercise1.py
@@ -8,3 +8,10 @@ elif age >= 12 and age < 18:
     print("Você pode assitir ao filme Super Mario Bros")
 else:
     print("Você pode assistir ao filme Invocação do Mal 4")
+
+# Tickets for movies:
+qtd_tickets = 0
+if qtd_tickets > 0:
+    print("Você pode comprar um ingresso para assistir ao seu filme")
+elif qtd_tickets == 0:
+    ("Não há mais ingressos disponíveis para o seu filme")
