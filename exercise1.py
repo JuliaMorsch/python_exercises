ad71fde9088cf4c283cc867c531c07e3e4a7d4b1
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-08 15:35:30 -0300

    The conditional for adults whatching movie

--- exercise1.py
+++ exercise1.py
@@ -5,4 +5,6 @@ age = int(input(" Digite a sua idade:"))
 if age < 12:
     print("Você pode assistir ao filme Lilo & Stich")
 elif age >= 12 and age < 18:
-    print("Você pode assitir ao filme Super Mario Bros") 
+    print("Você pode assitir ao filme Super Mario Bros")
+else age >= 18: 
+    print("Você pode assistr ao filme Invocação do Mal 4")
