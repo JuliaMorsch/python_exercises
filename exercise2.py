b648326790cbfa83550e9b9ba306520866cb0d2f
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-11 18:02:19 -0300

    Fix errors of condiotional rating

--- exercise2.py
+++ exercise2.py
@@ -2,18 +2,21 @@
 
 # Wellcome message
 print("Bem-vindo à classificação de filmes!")
-print("Você tem 5 filmes para avaqliar.")
+print("Você tem 5 filmes para avaliar.")
 print("Digite 0 a qualquer momento para sair.")
 
 movie = ["filme1", "filme2", "filme3", "filme4", "filme5"]
 
 for mov in movie:
-    rating = int(input(f"Como você avaliaria o {mov} de 1 a 5?:"))
-    if rating == 0:
+    rating = 6
+    while rating < 1 or rating > 5: 
+     rating = int(input(f"Como você avaliaria o {mov} de 1 a 5?:"))
+
+     if rating == 0:
         break
 
-    if rating < 1 or rating > 5:
+     if rating < 1 or rating > 5:
         print("Avalie o filme com uma nota válida.")
-    else:
+     else:
         print(f"Você avaliou o {mov} com a nota {rating}.")
 print("Obrigada por participar!")
