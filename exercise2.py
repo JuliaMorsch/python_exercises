8b00bc6fc994a6fc588cd56cce404fa80e76924d
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-11 17:49:24 -0300

    Include rating condional of movies

--- exercise2.py
+++ exercise2.py
@@ -8,9 +8,12 @@ print("Digite 0 a qualquer momento para sair.")
 movie = ["filme1", "filme2", "filme3", "filme4", "filme5"]
 
 for mov in movie:
-    print(mov)
     rating = int(input(f"Como você avaliaria o {mov} de 1 a 5?:"))
     if rating == 0:
-        print("Obrigada por participar!")
         break
 
+    if rating < 1 or rating > 5:
+        print("Avalie o filme com uma nota válida.")
+    else:
+        print(f"Você avaliou o {mov} com a nota {rating}.")
+print("Obrigada por participar!")
