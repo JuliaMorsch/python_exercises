a137750d8c8c3c203c91d457e7d42a92f0782f1b
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-18 15:43:22 -0300

    Include round grades

--- exercise3.py
+++ exercise3.py
@@ -7,3 +7,15 @@ def calculate_avg(grades):
     avg = sum_grades / len(grades)
     return avg
 
+# Lambda function that round the avarage to two decimal places
+
+round_avg = lambda media: round(media, 2)
+
+# Calaculate media of grades and print the result
+
+media = calculate_avg(grades)
+media_avg = round_avg(media)
+
+# Check the students' situation
+
+situation = "Aprovado" if media_avg >= 7 else "Reprovado"
