58425cdad5820068628654b8ad93a59ab854dfed
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-18 16:38:43 -0300

    Reiceive  values of discount and price

---
+++ exercise4.py
@@ -0,0 +1,11 @@
+# Algorithm to calculate the discount on a product
+
+# Receive the product value
+value = float(input("Informe o valor do produto que você deseja comprar: "))
+
+# Receive the discount percentage
+discount_per = int(input("Informe o percentual de desconto: "))
+
+#  Evaluates if the percentage is valid
+
+
