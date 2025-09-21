9a1c9fbcbf7d74e286e06576980b943a5d41b609
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-21 15:28:55 -0300

    Check if the value discount is valid and print the final value

--- exercise4.py
+++ exercise4.py
@@ -8,4 +8,11 @@ discount_per = int(input("Informe o percentual de desconto: "))
 
 #  Evaluates if the percentage is valid
 
+if discount_per < 0 or discount_per > 100:
+    print("Informe um percentual válido")
+else:
+    discount = value * (discount_per / 100)
 
+final_value = value - discount
+
+print(f"O valor final do produto é R$ {final_value:.2f}")
