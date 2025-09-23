c2603a2d29e8b0e739d8e5d4ff1fb24cea55d9f0
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-23 16:08:11 -0300

    Include guest lits

---
+++ exercise5.py
@@ -0,0 +1,9 @@
+# Create a new guest list of people didin't RSVP
+
+# guest list
+gust_list = ("Alice", "Frank", "Josh", "Peter", "Sam")
+
+# List of confirmated
+confirmated = ["Frank", "Sam"]
+
+# New list of people who didn't RSVP
