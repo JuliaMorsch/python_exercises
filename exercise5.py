693ac64390ff0cc42012ded25125d9423a67c454
Júlia Morsch <juliamorsh17@gmail.com>
2025-09-23 16:19:35 -0300

    Logic for include guests who didn't RSVP

--- exercise5.py
+++ exercise5.py
@@ -1,9 +1,16 @@
 # Create a new guest list of people didin't RSVP
 
 # guest list
-gust_list = ("Alice", "Frank", "Josh", "Peter", "Sam")
+guest_list = ("Alice", "Frank", "Josh", "Peter", "Sam")
 
 # List of confirmated
 confirmated = ["Frank", "Sam"]
 
 # New list of people who didn't RSVP
+not_rsvp = [guests for guests in guest_list if guests not in confirmated]
+
+# Print the new list
+print("Convidados que ainda não confirmaram: ")
+for guest in not_rsvp:
+    print(guest)
+    
