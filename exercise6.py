7725956066d4ab5db30f46b45aff911091e2aaf0
Júlia Morsch <juliamorsh17@gmail.com>
2025-10-04 19:10:12 -0300

    fix sintax erros

--- exercise6.py
+++ exercise6.py
@@ -5,19 +5,19 @@ import numpy as np
 participants = [
     {
         'nome': "Agatha",
-        'localizacao':"Brasil"
+        'localizacao':"Brasil",
         'afiliacao': "UFSC",
         'interesses': ['Física', 'Astronomia']
-    }
+    },
     {
         'nome': "Hugo",
-        'localizacao':"Mexico"
+        'localizacao':"Mexico",
         'afiliacao': "Intituto A",
         'interesses': ['Gastrônomia', 'Astronomia']
-    }
+    },
     {
         'nome': "Agatha",
-        'localizacao':"Argentina"
+        'localizacao':"Argentina",
         'afiliacao': "Instituto C",
         'interesses': ['Geografia', 'Engenharia']
     }
@@ -33,3 +33,13 @@ for participant in participants:
     if affil not in affiliations:
         affiliations[affil] = []
     affiliations[affil].append(participant['nome'])
+
+# Using NumPy to alaize interest of area
+interest_areas = np.array([interest for participant in participants for interest in participant['interesses']])
+unique_interests, counts = np.unique(interest_areas, return_counts=True)
+popular_area = unique_interests[np.argmax(counts)]
+
+#Results
+print("Regions represented:", regions)
+print("Affiliations categorized:", affiliations)
+print("Most popular area of interest:", popular_area)
