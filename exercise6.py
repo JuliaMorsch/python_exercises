c0ab420d417f6cd12efa61dfe172eb0f18bdacfc
Júlia Morsch <juliamorsh17@gmail.com>
2025-10-04 18:37:26 -0300

    Add some partcipants

---
+++ exercise6.py
@@ -0,0 +1,24 @@
+# A program that analyzes data from participants in a scientific event
+import numpy as np
+
+# participants data
+participants = [
+    {
+        'nome': "Agatha",
+        'localizacao':"Brasil"
+        'afiliacao': "UFSC",
+        'interesses': ['Física', 'Astronomia']
+    }
+    {
+        'nome': "Hugo",
+        'localizacao':"Mexico"
+        'afiliacao': "Intituto A",
+        'interesses': ['Gastrônomia', 'Astronomia']
+    }
+    {
+        'nome': "Agatha",
+        'localizacao':"Brasil"
+        'afiliacao': "UFSC",
+        'interesses': ['Geografia', 'Engenharia']
+    }
+]
